8c1e6b90e76bb4a1b98e4783b5d02c5267fb3bc4
Marius Hegele <marius.hegele@mail.de>
2025-01-18 17:18:57 +0100

    Day 5, part 2: mapping the source range through to final destination range

--- aoc_2023/lib/day5.ex
+++ aoc_2023/lib/day5.ex
@@ -3,13 +3,7 @@ defmodule Day5 do
     {seeds_input, maps_input} = split_input(input)
     seeds = Seeds.from_list(seeds_input)
     maps = SequentialRangeMaps.new(maps_input)
-    locations(seeds, maps)
-    |> Enum.min()
-  end
-  def closest_location_of_seed_ranges(input) do
-    {seeds_input, maps_input} = split_input(input)
-    seeds = Seeds.from_ranges(seeds_input)
-    maps = SequentialRangeMaps.new(maps_input)
+
     locations(seeds, maps)
     |> Enum.min()
   end
@@ -19,10 +13,21 @@ defmodule Day5 do
     |> Enum.map(fn seed -> SequentialRangeMaps.get(maps, seed) end)
   end
 
+  def closest_location_of_seed_ranges(input) do
+    {seeds_input, maps_input} = split_input(input)
+    seed_ranges = Seeds.parse_ranges(seeds_input)
+    maps = SequentialRangeMaps.new(maps_input)
+    fdm = FinalDestinationMap.new(maps)
+
+    FinalDestinationMap.closest_location_of_seed_ranges(fdm, seed_ranges)
+  end
+
   defp split_input(seeds_and_maps) do
-    [seeds_input | maps_lines] = seeds_and_maps
+    [seeds_input | maps_lines] =
+      seeds_and_maps
       |> String.trim()
       |> String.split("\n")
+
     maps_input = Enum.join(maps_lines, "\n")
 
     {seeds_input, maps_input}
@@ -35,23 +40,20 @@ defmodule Seeds do
     |> parse_numbers()
   end
 
-
-  def from_ranges(seeds_line) do
+  def parse_ranges(seeds_line) do
     numbers = parse_numbers(seeds_line)
     num_pairs = div(Enum.count(numbers), 2)
+
     0..(num_pairs - 1)
     |> Enum.map(fn i -> {Enum.at(numbers, 2 * i + 0), Enum.at(numbers, 2 * i + 1)} end)
-    |> IO.inspect()
-    |> Enum.reduce([], fn {first_seed, seed_count}, seeds -> 
-      Enum.to_list(first_seed..(first_seed + seed_count - 1)) ++ seeds end)
-      |> IO.inspect()
   end
 
   defp parse_numbers(seeds_line) do
     [_, numbers] = Regex.run(~r/seeds:\s+(.+)/, seeds_line)
-    numbers 
-    |> String.trim() 
-    |> String.split() 
+
+    numbers
+    |> String.trim()
+    |> String.split()
     |> Enum.map(&String.to_integer/1)
   end
 end
@@ -69,25 +71,130 @@ defmodule SequentialRangeMaps do
   end
 end
 
+defmodule FinalDestinationMap do
+  def new(sequential_range_maps) do
+    reversed = Enum.reverse(sequential_range_maps)
+
+    [last | _rest_reversed] = reversed
+    final_destination_map = [{0, RangeMap.max_dest_value(last)}]
+
+    {ranges, _acc} =
+      reversed
+      |> Enum.reduce(final_destination_map, fn map, final_destination_map ->
+        RangeMap.break_into(map, final_destination_map)
+      end)
+      |> Enum.map_reduce(0, fn {start, stop}, index ->
+        {{index, {start, stop}}, index + stop - start + 1}
+      end)
+
+    ranges
+  end
+
+  def closest_location_of_seed_ranges(fdm, seed_ranges) do
+    seed_ranges
+    |> Enum.map(fn seed_range -> closest_location_of_seed_range(fdm, seed_range) end)
+    |> Enum.min()
+  end
+
+  def closest_location_of_seed_range(fdm, {seed_start, delta}) do
+    last = Enum.at(fdm, -1)
+
+    seed_stop = seed_start + delta
+
+    {index, {start, _stop}} =
+      fdm
+      |> Enum.find(last, fn {_index, {start, stop}} ->
+        PlainRange.ranges_intersect({start, stop}, {seed_start, seed_stop})
+      end)
+
+    index + max(seed_start, start) - start
+  end
+
+  def get(fdm, key) do
+    last = Enum.at(fdm, -1)
+
+    {index, {start, _stop}} =
+      fdm
+      |> Enum.find(last, fn {_index, {start, stop}} -> start <= key and key <= stop end)
+
+    index + (key - start)
+  end
+end
+
 defmodule RangeMap do
   def new(map_label_and_ranges) do
-    [_label | ranges] = map_label_and_ranges 
-    |> String.trim()
-    |> String.split("\n")
+    [_label | ranges] =
+      map_label_and_ranges
+      |> String.trim()
+      |> String.split("\n")
 
     ranges
     |> Stream.map(&MappedRange.new/1)
-    |> Enum.sort(fn range1, range2 -> 
-      MappedRange.start_source(range1) <= MappedRange.start_source(range2) end)
+    |> Enum.sort(fn range1, range2 ->
+      MappedRange.start_source(range1) <= MappedRange.start_source(range2)
+    end)
   end
 
   def get(map, key) do
     range = Enum.find(map, fn range -> MappedRange.in_source_range(range, key) end)
+
     case range do
       nil -> key
       range -> MappedRange.map(range, key)
     end
+  end
 
+  def sort_by_destination(map) do
+    map
+    |> Enum.sort(fn range1, range2 ->
+      MappedRange.start_dest(range1) <= MappedRange.start_dest(range2)
+    end)
+  end
+
+  def max_dest_value(map) do
+    map |> Stream.map(&MappedRange.max_dest_value/1) |> Enum.max()
+  end
+
+  def break_into(map, final_destination_map) do
+    # {15, 51}, {52, 53}, {0, 14}
+    # dest_ranges {50, 51}, {52, 99}
+    # src_ranges  {98, 99}, {50, 97}
+
+    # goal: {15, 49}, {98, 99}, {50, 51}, {0, 14}, ( {52, 97} )
+
+    # go over final_destination_map
+    # {15, 51} -> find relevant ranges: {50, 51} subset   => break up: {15, 49}, {50, 51} and replace: {15, 49}, {98, 99}
+    # {52, 53} -> find relevant ranges: {52, 99} superset => keep {52, 53} and replace: {50, 51}
+    # { 0, 14} -> find relevant ranges: none => keep {0, 14}
+
+    # left over: dest {54, 99} = {52, 97} (not needed?)
+
+    dest_sorted_map = sort_by_destination(map)
+
+    final_destination_map
+    |> Enum.map(fn final_dest_range ->
+      {leftover_final_dest_range, source_ranges} =
+        Enum.filter(dest_sorted_map, fn range ->
+          MappedRange.intersects_destination(range, final_dest_range)
+        end)
+        |> Enum.reduce({final_dest_range, []}, fn range,
+                                                  {leftover_final_dest_range, all_source_ranges} ->
+          {leftover_final_dest_range, source_ranges} =
+            MappedRange.split_up_into_source(range, leftover_final_dest_range)
+
+          {leftover_final_dest_range, all_source_ranges ++ source_ranges}
+        end)
+
+      if leftover_final_dest_range != nil do
+        List.insert_at(source_ranges, -1, leftover_final_dest_range)
+      else
+        source_ranges
+      end
+    end)
+    |> List.flatten()
+
+    # {0, 36} -> find in {0, 53} -> index = 0 -> add [  ]
+    # {0, 36}, {37, 38}, {39, 53}
   end
 end
 
@@ -108,11 +215,88 @@ defmodule MappedRange do
     range.start_source <= number and number < range.start_source + range.length
   end
 
+  def intersects_destination(range, {dest_range_start, dest_range_end}) do
+    {a, b} = dest_range(range)
+    {c, d} = {dest_range_start, dest_range_end}
+    PlainRange.ranges_intersect({a, b}, {c, d})
+  end
+
+  def source_range_of_dest_intersection(range, {dest_range_start, dest_range_end}) do
+    {a, b} = dest_range(range)
+    {c, d} = {dest_range_start, dest_range_end}
+
+    {intersection_start, intersection_end} = {max(a, c), min(b, d)}
+    {invert_map(range, intersection_start), invert_map(range, intersection_end)}
+  end
+
+  def split_up_into_source(range, {dest_range_start, dest_range_end}) do
+    {a, b} = dest_range(range)
+    {c, d} = {dest_range_start, dest_range_end}
+
+    # scenario 1: [a    b][e  ?  f]      -> range: [c b] leftover: [b d]
+    #                 [c    d]
+    #
+    # scenario 2: [a       b] -> [c d]  -> range: [c  d] leftover: none
+    #               [c  d]
+    #
+    # scenario 3:      [a    b] -> ranges: [c, a - 1] (Id.) + [a  d] leftover: none
+    #             [c    d]
+    #
+    # scenario 4:    [a  b]     ->  ranges: [c,  a - 1] (Id.) + [a  b] leftover: [b + 1, d]
+    #             [c       d]
+
+    {intersection_start, intersection_end} = {max(a, c), min(b, d)}
+    source_range = {invert_map(range, intersection_start), invert_map(range, intersection_end)}
+
+    source_ranges =
+      if c < a do
+        [{c, a - 1}, source_range]
+      else
+        [source_range]
+      end
+
+    leftover =
+      if b < d do
+        {b + 1, d}
+      else
+        nil
+      end
+
+    {leftover, source_ranges}
+  end
+
   def start_source(range) do
     range.start_source
   end
 
+  def start_dest(range) do
+    range.start_dest
+  end
+
+  def dest_range(range) do
+    {range.start_dest, range.start_dest + range.length - 1}
+  end
+
   def map(range, key) do
     key - range.start_source + range.start_dest
+  end
+
+  def invert_map(range, key) do
+    key - range.start_dest + range.start_source
+  end
+
+  def max_dest_value(range) do
+    range.start_dest + range.length
+  end
+end
+
+defmodule PlainRange do
+  def ranges_intersect({a, b}, {c, d}) do
+    # (a, b) & (c, d)
+    # negative:
+    # [a  b]
+    #        [c d]
+    #             [a b]
+    not (b < c or a > d)
   end
 end
--- aoc_2023/test/day5_test.exs
+++ aoc_2023/test/day5_test.exs
@@ -10,20 +10,19 @@ defmodule TestDay5 do
     assert not MappedRange.in_source_range(range, 40 + 48)
   end
 
-  @test_map """
+  @first_map """
   seed-to-soil map:
   50 98 2
   52 50 48
   """
 
   test "test range map" do
-    map = RangeMap.new(@test_map)
+    map = RangeMap.new(@first_map)
     assert RangeMap.get(map, 79) == 81
     assert RangeMap.get(map, 14) == 14
     assert RangeMap.get(map, 55) == 57
     assert RangeMap.get(map, 13) == 13
   end
-
 
   @test_sequential_maps """
   seed-to-soil map:
@@ -36,13 +35,87 @@ defmodule TestDay5 do
   39 0 15
   """
 
+  @last_map """
+  soil-to-fertilizer map:
+  0 15 37
+  37 52 2
+  39 0 15
+  """
+
   test "test SequentialRangeMaps" do
     maps = SequentialRangeMaps.new(@test_sequential_maps)
-    assert SequentialRangeMaps.get(maps, 79) == 81 # 79 -> 81 -> 81
-    assert SequentialRangeMaps.get(maps, 14) == 53 # 14 -> 14 -> 53
-    assert SequentialRangeMaps.get(maps, 55) == 57 # 55 -> 57 -> 57
-    assert SequentialRangeMaps.get(maps, 13) == 52 # 13 -> 13 -> 52
+    # 79 -> 81 -> 81
+    assert SequentialRangeMaps.get(maps, 79) == 81
+    # 14 -> 14 -> 53
+    assert SequentialRangeMaps.get(maps, 14) == 53
+    # 55 -> 57 -> 57
+    assert SequentialRangeMaps.get(maps, 55) == 57
+    # 13 -> 13 -> 52
+    assert SequentialRangeMaps.get(maps, 13) == 52
   end
+
+  test "test reduce SequentialRangeMaps " do
+    maps = SequentialRangeMaps.new(@test_sequential_maps)
+    # fertilizer   0 ... 34   35 36   37 38   39 ... 53   54 55 ... 99
+    # soil dest   15 ... 49   50 51 | 52 53 |  0 ... 14 | 54 55 ... 99
+    # seed        15 ... 49 | 98 99 | 50 51 |  0 ... 14 | 52 53 ... 97 
+
+    intermediate = [
+      {15, 51},
+      {52, 53},
+      {0, 14},
+      {54, 99}
+    ]
+
+    final_destination_map = [
+      {15, 49},
+      {98, 99},
+      {50, 51},
+      {0, 14},
+      {52, 97}
+    ]
+
+    first_map = RangeMap.new(@first_map)
+    assert RangeMap.break_into(first_map, intermediate) == final_destination_map
+
+    last_map = RangeMap.new(@last_map)
+    assert RangeMap.break_into(last_map, [{0, 99}]) == intermediate
+
+    fdm = FinalDestinationMap.new(maps)
+
+    assert fdm == [
+             {0, {15, 49}},
+             {35, {98, 99}},
+             {37, {50, 51}},
+             {39, {0, 14}},
+             {54, {52, 52}}
+           ]
+
+    # 79 -> 81 -> 81
+    assert FinalDestinationMap.get(fdm, 79) == 81
+    # 14 -> 14 -> 53
+    assert FinalDestinationMap.get(fdm, 14) == 53
+    # 55 -> 57 -> 57
+    assert FinalDestinationMap.get(fdm, 55) == 57
+    # 13 -> 13 -> 52
+    assert FinalDestinationMap.get(fdm, 13) == 52
+
+    0..99
+    |> Enum.each(fn i ->
+      assert FinalDestinationMap.get(fdm, i) == SequentialRangeMaps.get(maps, i)
+    end)
+
+    seed_ranges = [
+      # -> 0
+      {11, 4},
+      # -> 1
+      {16, 4}
+    ]
+
+    assert FinalDestinationMap.closest_location_of_seed_ranges(fdm, seed_ranges) == 0
+  end
+
+  # 79, 80, .. 92
 
   @test_task """
   seeds: 79 14 55 13
@@ -89,11 +162,10 @@ defmodule TestDay5 do
 
   @actual_task File.read!("data/day5.txt")
   test "test actual task with seed list" do
-    assert Day5.closest_location(@actual_task) == 525792406
+    assert Day5.closest_location(@actual_task) == 525_792_406
   end
 
-  @tag :pending
   test "test actual task with seed range" do
-    assert Day5.closest_location_of_seed_ranges(@actual_task) == 525792406
+    assert Day5.closest_location_of_seed_ranges(@actual_task) == 525_792_406
   end
 end
