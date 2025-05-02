eba5340909d6538fb03f4ae324e0588f37daf061
Marius Hegele <marius.hegele@mail.de>
2025-05-02 07:22:33 +0200

    AoC Day 8, pt. 2

--- aoc_2023/lib/day8.ex
+++ aoc_2023/lib/day8.ex
@@ -1,28 +1,48 @@
 defmodule LRMap do
-  def num_steps(input) do
+  def num_steps(input, simultaneous \\ false) do
     {instructions_input, map_input} = InputParser.split_into_header_and_rest(input)
     instructions = parse_instructions(instructions_input)
     map = parse_map(map_input)
 
-    search_zzz(map, instructions, "AAA", 0)
+    {initial_nodes, finish_suffix} =
+      if simultaneous do
+        {nodes_ending_with(map, "A"), "Z"}
+      else
+        {["AAA"], "ZZZ"}
+      end
+
+    initial_nodes
+    |> Enum.map(fn node ->
+      search_finish_nodes(map, instructions, node, finish_suffix, 0)
+    end)
+    |> Numerics.lcm()
   end
 
-  defp search_zzz(map, instructions, active_node, i) do
-    if active_node == "ZZZ" do
-      i
+  defp search_finish_nodes(map, instructions, active_node, finish_suffix, num_steps) do
+    if String.ends_with?(active_node, finish_suffix) do
+      num_steps
     else
-      j = rem(i, Enum.count(instructions))
-      instruction = Enum.at(instructions, j)
-      {left_node, right_node} = Map.get(map, active_node)
+      i = rem(num_steps, Enum.count(instructions))
+      instruction = Enum.at(instructions, i)
 
-      new_active_node =
-        case instruction do
-          "L" -> left_node
-          "R" -> right_node
-        end
-
-      search_zzz(map, instructions, new_active_node, i + 1)
+      new_active_node = step_node(map, active_node, instruction)
+      search_finish_nodes(map, instructions, new_active_node, finish_suffix, num_steps + 1)
     end
+  end
+
+  defp step_node(map, active_node, instruction) do
+    {left_node, right_node} = Map.get(map, active_node)
+
+    case instruction do
+      "L" -> left_node
+      "R" -> right_node
+    end
+  end
+
+  defp nodes_ending_with(map, suffix) do
+    Map.keys(map)
+    |> Enum.filter(fn key -> String.ends_with?(key, suffix) end)
+    |> MapSet.new()
   end
 
   defp parse_instructions(input) do
@@ -39,7 +59,30 @@ defmodule LRMap do
   end
 
   def parse_map_line(line) do
-    [_line, loc, left, right] = Regex.run(~r/([A-Z]+)\s=\s\(([A-Z]+),\s([A-Z]+)\)/, line)
+    line_regex = ~r/([A-Z0-9]+)\s=\s\(([A-Z0-9]+),\s([A-Z0-9]+)\)/
+    [_line, loc, left, right] = Regex.run(line_regex, line)
     {loc, {left, right}}
+  end
+end
+
+defmodule Numerics do
+  def gcd(d, 0) do
+    d
+  end
+
+  def gcd(a, b) when a >= b do
+    gcd(b, rem(a, b))
+  end
+
+  def gcd(a, b) when b > a do
+    gcd(a, rem(b, a))
+  end
+
+  def lcm(a, b) do
+    abs(a) * div(abs(b), gcd(a, b))
+  end
+
+  def lcm([head | tail]) do
+    Enum.reduce(tail, head, &lcm(&1, &2))
   end
 end
--- aoc_2023/test/day8_test.exs
+++ aoc_2023/test/day8_test.exs
@@ -21,6 +21,19 @@ defmodule TestDay8 do
     ZZZ = (ZZZ, ZZZ)
   """
 
+  @example_input_pt2 """
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+  """
+
   @actual_input File.read!("data/day8.txt")
 
   test "test example input" do
@@ -31,8 +44,42 @@ defmodule TestDay8 do
     assert LRMap.num_steps(@example_input2) == 6
   end
 
-  @tag :pending
+  test "test example input pt. 2" do
+    assert LRMap.num_steps(@example_input_pt2, true) == 6
+  end
+
   test "test actual input" do
-    assert LRMap.num_steps(@actual_input, true) == 8
+    assert LRMap.num_steps(@actual_input) == 18673
+  end
+
+  test "test actual input pt. 2" do
+    assert LRMap.num_steps(@actual_input, true) == 17_972_669_116_327
+  end
+end
+
+defmodule TestNumerics do
+  use ExUnit.Case
+
+  test "test greatest common divisor" do
+    assert Numerics.gcd(48, 18) == 6
+    assert Numerics.gcd(1, 1) == 1
+    assert Numerics.gcd(2, 2) == 2
+    assert Numerics.gcd(4, 2) == 2
+    assert Numerics.gcd(9, 6) == 3
+    assert Numerics.gcd(64, 8) == 8
+    assert Numerics.gcd(64, 56) == 8
+  end
+
+  test "test least common multiple" do
+    assert Numerics.lcm(21, 6) == 42
+    assert Numerics.lcm(1, 1) == 1
+    assert Numerics.lcm(2, 2) == 2
+    assert Numerics.lcm(4, 2) == 4
+    assert Numerics.lcm(9, 6) == 18
+    assert Numerics.lcm(64, 8) == 64
+
+    assert Numerics.lcm([21, 6]) == 42
+    assert Numerics.lcm([21, 6, 42]) == 42
+    assert Numerics.lcm([21, 6, 84]) == 84
   end
 end
